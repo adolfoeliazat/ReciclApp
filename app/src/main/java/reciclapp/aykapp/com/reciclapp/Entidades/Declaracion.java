40e48c42fc2f08a025258e74775e5b300e0786cf
Alejandro <alejandrosuns@gmail.com>
2016-03-01 20:58:47 -0300

    Inicial

---
+++ app/src/main/java/reciclapp/aykapp/com/reciclapp/Entidades/Declaracion.java
@@ -0,0 +1,7 @@
+package reciclapp.aykapp.com.reciclapp.Entidades;
+
+/**
+ * Created by Alejandro on 01/03/2016.
+ */
+public class Declaracion {
+}
---
+++ app/src/main/java/reciclapp/aykapp/com/reciclapp/Fragments/FragmentInicial.java
@@ -0,0 +1,7 @@
+package reciclapp.aykapp.com.reciclapp.Fragments;
+
+/**
+ * Created by Alejandro on 01/03/2016.
+ */
+public class FragmentInicial {
+}
