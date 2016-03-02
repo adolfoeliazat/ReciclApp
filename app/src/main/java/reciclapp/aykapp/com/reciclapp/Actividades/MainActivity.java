e19c58f367786349854a4905cdd3cd113dd39bd0
Alejandro <alejandrosuns@gmail.com>
2016-03-01 21:04:26 -0300

    Inicial

--- app/src/main/java/reciclapp/aykapp/com/reciclapp/Actividades/MainActivity.java
+++ app/src/main/java/reciclapp/aykapp/com/reciclapp/Actividades/MainActivity.java
@@ -1,4 +1,4 @@
-package reciclapp.aykapp.com.reciclapp;
+package reciclapp.aykapp.com.reciclapp.Actividades;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import reciclapp.aykapp.com.reciclapp.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
---
+++ app/src/main/java/reciclapp/aykapp/com/reciclapp/Adapters/AdapterInicial.java
@@ -0,0 +1,7 @@
+package reciclapp.aykapp.com.reciclapp.Adapters;
+
+/**
+ * Created by Alejandro on 01/03/2016.
+ */
+public class AdapterInicial {
+}
